0c2e852677ed2b386fb2feae1ec6d74096e73323
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-12-28 19:02:08 +0900

    edit: SaleItem 에 creator 필드 생성

--- core/models.py
+++ core/models.py
@@ -2,15 +2,14 @@ import uuid
 from django.db import models
 from config.utils import compress_image, upload_path
 from common.models import CommonModel
-
-# from user.models import User
+from user.models import User
 
 
 class SaleItem(CommonModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False)
-    # creator = models.ForeignKey(
-    #     User, null=True, related_name="sales_items", on_delete=models.CASCADE
-    # )
+    creator = models.ForeignKey(
+        User, null=True, related_name="sales_items", on_delete=models.CASCADE
+    )
     title = models.CharField(max_length=50, null=True, blank=True)
     content = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
