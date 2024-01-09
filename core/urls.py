fdce78d9bd4dc6ad07f276a8c2217cb7e6c4dd98
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-01-09 17:46:33 +0900

    add: SaleItemDetail

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SaleItemListCreate
+from .views import SaleItemListCreate, SaleItemDetail
 
 urlpatterns = [
     path("sale-items", SaleItemListCreate.as_view()),
+    path("sale-items/<str:pk>", SaleItemDetail.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from config.permissions import IsCreatorPermission
 from rest_framework.parsers import MultiPartParser
 from .serializers import SaleItemSerializer
 from .models import SaleItem
@@ -14,3 +15,11 @@ class SaleItemListCreate(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
+
+
+class SaleItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    parser_classes = [MultiPartParser]
+    serializer_class = SaleItemSerializer
+    permission_classes = [IsCreatorPermission]
+    queryset = SaleItem.objects.all()
+    lookup_field = "pk"
