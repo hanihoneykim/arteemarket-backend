2a555c60377271272a543f3db339b0357960360e
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-01-09 16:10:18 +0900

    add: FundingItem

--- core/models.py
+++ core/models.py
@@ -2,7 +2,6 @@ import uuid
 from django.db import models
 from config.utils import compress_image, upload_path
 from common.models import CommonModel
-from user.models import User
 
 
 class Category(models.Model):
@@ -13,13 +12,48 @@ class Category(models.Model):
 class SaleItem(CommonModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False)
     creator = models.ForeignKey(
-        User, null=True, related_name="sales_items", on_delete=models.CASCADE
+        "user.User", null=True, related_name="sales_items", on_delete=models.CASCADE
     )
     title = models.CharField(max_length=50, null=True, blank=True)
     content = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(null=True, blank=True, upload_to=upload_path)
-    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name="sales_items", null=True, on_delete=models.CASCADE
+    )
+
+    def save(self, *args, **kwargs):
+        if self.pk and self.image:
+            try:
+                original = SaleItem.objects.get(pk=self.pk)
+                if original.image != self.image:
+                    self.image = compress_image(self.image, size=(500, 500))
+            except SaleItem.DoesNotExist:
+                self.image = compress_image(self.image, size=(500, 500))
+            except:
+                pass
+        super().save(*args, **kwargs)
+
+
+class FundingItem(CommonModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, null=False, blank=False)
+    creator = models.ForeignKey(
+        "user.User", null=True, related_name="funding_items", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=50, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    goal_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="목표 금액"
+    )
+    current_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0, verbose_name="현재 모금액"
+    )
+    end_date = models.DateTimeField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to=upload_path)
+    category = models.ForeignKey(
+        Category, related_name="funding_items", null=True, on_delete=models.CASCADE
+    )
 
     def save(self, *args, **kwargs):
         if self.pk and self.image:
