7d0da2c72505e557c2b8ca692b3bb83982528827
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-01-15 12:33:20 +0900

    add: UserSignUp

--- user/views.py
+++ user/views.py
@@ -6,8 +6,38 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from core.models import FundingItem, SaleItem
-from .serializers import ParticipantSerializer, PurchaseSerializer
-from .models import Participant, Purchase
+from .serializers import ParticipantSerializer, PurchaseSerializer, UserSerializer
+from .models import Participant, Purchase, User, AuthToken
+
+
+class UserSignUp(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        create user using UserSerializer, save hashed password, and return token
+        """
+        # 기입한 정보가 이미 존재하는 경우 오류 메세지 주기
+        if User.objects.filter(email=request.data["email"]).exists():
+            return Response(
+                data={"status": "FAILED", "message": "이미 존재하는 이메일입니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if User.objects.filter(nickname=request.data["nickname"]).exists():
+            return Response(
+                data={"status": "FAILED", "message": "이미 존재하는 닉네임입니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        user = serializer.instance
+        token = AuthToken.objects.create(user=user)
+        user.set_password(request.data.get("password"))
+        user.save()
+        return Response(
+            {"id": str(user.id), "token": str(token.id)}, status=status.HTTP_201_CREATED
+        )
 
 
 class ParticipantListCreate(generics.ListCreateAPIView):
