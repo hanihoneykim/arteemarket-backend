a64b48a3ae32f389a3381489ec13e8e627806bc6
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2024-01-22 15:21:18 +0900

    add: Notice model

--- core/models.py
+++ core/models.py
@@ -102,3 +102,12 @@ class FundingItem(CommonModel):
 
 class MainPageSlideBanner(models.Model):
     image = models.ImageField(upload_to=upload_path, null=True, blank=True)
+
+
+class Notice(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, auto_created=True)
+    title = models.CharField(max_length=120, null=False, blank=False)
+    content = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
